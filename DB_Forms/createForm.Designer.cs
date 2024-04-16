55a6a11abc317bebfb5afe6b071c5823b8c04e93
Qullfie <lotarev200445@gmail.com>
2024-04-16 15:14:20 +0300

    Почти доделал create

--- DB_Forms/createForm.Designer.cs
+++ DB_Forms/createForm.Designer.cs
@@ -28,23 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.testBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // testBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(45, 91);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 125);
-            this.textBox1.TabIndex = 0;
+            this.testBox.Location = new System.Drawing.Point(33, 323);
+            this.testBox.Name = "testBox";
+            this.testBox.Size = new System.Drawing.Size(218, 22);
+            this.testBox.TabIndex = 0;
             // 
             // createForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(293, 357);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.testBox);
             this.Name = "createForm";
             this.Text = "createForm";
             this.Load += new System.EventHandler(this.createForm_Load);
@@ -55,6 +54,6 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox testBox;
     }
 }
--- DB_Forms/createForm.cs
+++ DB_Forms/createForm.cs
@@ -1,15 +1,20 @@
 ﻿using Npgsql;
 using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
+
 
 namespace DB_Forms
 {
     public partial class createForm : Form
     {
+        static List<Tuple<Label, TextBox>> fieldList = new List<Tuple<Label, TextBox>>();
+        private string tableName;
         public createForm(string tableName)
         {
             InitializeComponent();
             showCreationInfo(tableName);
+            this.tableName = tableName;
         }
 
         private void createForm_Load(object sender, EventArgs e)
@@ -19,18 +24,76 @@ namespace DB_Forms
 
         private void showCreationInfo(string tableName)
         {
-            using (var connection = DataBaseConnection.connection)
+            int labelWidth = 75;
+            int textBoxWidth = 100;
+            int spacing = 10;
+            int currentY = 20;
+            var connection = DataBaseConnection.connection;
+            using (var cmd = new NpgsqlCommand($"SELECT column_name FROM information_schema.columns WHERE table_name = '{tableName}'", connection))
             {
-                using (var cmd = new NpgsqlCommand($"SELECT column_name FROM information_schema.columns WHERE table_name = {tableName}", connection))
+                using (var reader = cmd.ExecuteReader())
                 {
-                    using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
                     {
-                        while (reader.Read())
-                        {
-                            textBox1.AppendText(reader.GetString(0));
-                        }
+                        //Создание лейблов
+                        Label label = new Label();
+                        label.Text = reader.GetString(0);
+                        label.Location = new System.Drawing.Point(25, currentY);
+                        label.Size = new System.Drawing.Size(labelWidth, label.Height);
+                        this.Controls.Add(label);
+
+                        //Создание текстбоксов
+
+                        TextBox textbox = new TextBox();
+                        textbox.Location = new System.Drawing.Point(100,currentY);
+                        textbox.Size = new System.Drawing.Size(textBoxWidth, textbox.Height);
+                        this.Controls.Add(textbox);
+
+                        fieldList.Add(new Tuple <Label, TextBox> (label, textbox));
+                        currentY += label.Height + spacing;
+
                     }
                 }
+            }
+
+            //Создание кнопки "добавить"
+            Button createBtn = new Button();
+            createBtn.Text = "create";
+            createBtn.Location = new System.Drawing.Point(50, currentY);
+            createBtn.Size = new System.Drawing.Size(100, createBtn.Height);
+            Controls.Add(createBtn);
+
+            createBtn.Click += new EventHandler(createBtn_click);
+
+        }
+
+        private void createBtn_click(object sender, EventArgs e)
+        {
+            string createCommand = $"INSERT INTO {tableName} VALUES (";
+            var parameters = new List<NpgsqlParameter>();
+            int parameterIndex = 1;
+            foreach (var field in fieldList)
+            {
+                createCommand += $"@{parameterIndex},";
+                parameters.Add(new NpgsqlParameter($"@{parameterIndex}", field.Item2.Text));
+                parameterIndex++;
+            }
+            string newstr = createCommand.Remove(createCommand.Length - 1) + ")";
+            try
+            {
+                using (var command = new NpgsqlCommand(newstr, DataBaseConnection.connection))
+                {
+                    //Добавление параметров в команду
+                    foreach (var parameter in parameters)
+                    {
+                        command.Parameters.Add(parameter);
+                    }
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
