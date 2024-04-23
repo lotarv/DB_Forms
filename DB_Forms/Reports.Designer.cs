11fe3c24f64b1178f9fa336934f6753f86fb46e7
lotarv <lotarev200445@gmail.com>
2024-04-23 16:16:34 +0300

    Закончил отчеты: добавил paginated view и режим read

--- DB_Forms/Reports.Designer.cs
+++ DB_Forms/Reports.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nextPageBtn = new System.Windows.Forms.Button();
+            this.prevPageBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +59,86 @@
             this.dataGridView1.Size = new System.Drawing.Size(560, 156);
             this.dataGridView1.TabIndex = 1;
             // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(357, 290);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(106, 28);
+            this.deleteBtn.TabIndex = 9;
+            this.deleteBtn.Text = "delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(235, 290);
+            this.updateBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(106, 28);
+            this.updateBtn.TabIndex = 8;
+            this.updateBtn.Text = "update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(567, 290);
+            this.readBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(106, 28);
+            this.readBtn.TabIndex = 10;
+            this.readBtn.Text = "read";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(113, 290);
+            this.createBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(106, 28);
+            this.createBtn.TabIndex = 11;
+            this.createBtn.Text = "create";
+            this.createBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(377, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // nextPageBtn
+            // 
+            this.nextPageBtn.Location = new System.Drawing.Point(428, 251);
+            this.nextPageBtn.Name = "nextPageBtn";
+            this.nextPageBtn.Size = new System.Drawing.Size(35, 23);
+            this.nextPageBtn.TabIndex = 13;
+            this.nextPageBtn.Text = ">";
+            this.nextPageBtn.UseVisualStyleBackColor = true;
+            this.nextPageBtn.Click += new System.EventHandler(this.nextPageBtn_Click);
+            // 
+            // prevPageBtn
+            // 
+            this.prevPageBtn.Location = new System.Drawing.Point(341, 251);
+            this.prevPageBtn.Name = "prevPageBtn";
+            this.prevPageBtn.Size = new System.Drawing.Size(30, 23);
+            this.prevPageBtn.TabIndex = 14;
+            this.prevPageBtn.Text = "<";
+            this.prevPageBtn.UseVisualStyleBackColor = true;
+            this.prevPageBtn.Click += new System.EventHandler(this.prevPageBtn_Click);
+            // 
             // Reports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prevPageBtn);
+            this.Controls.Add(this.nextPageBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.readBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Reports";
@@ -72,5 +154,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button nextPageBtn;
+        private System.Windows.Forms.Button prevPageBtn;
     }
 }
--- DB_Forms/Reports.cs
+++ DB_Forms/Reports.cs
@@ -1,34 +1,75 @@
 ﻿using Npgsql;
 using System;
 using System.Data;
+using System.Security.AccessControl;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace DB_Forms
 {
     public partial class Reports : Form
     {
+        int pageSize = 5; // Количество записей на странице
+        int currentPage = 1; // Текущая страница
         public Reports()
         {
             InitializeComponent();
             dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            createBtn.Enabled = false;
+            updateBtn.Enabled = false;
+            deleteBtn.Enabled = false;
+
+            textBox1.ReadOnly = true;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+
         }
 
         private void Reports_Load(object sender, EventArgs e)
         {
-            using (var connection = DataBaseConnection.connection)
+            
+        }
+
+        private void showReport()
+        {
+
+            string sqlQuery = $"SELECT * FROM blackbox LIMIT {pageSize} OFFSET {(currentPage - 1) * pageSize}";
+            using (var cmd = new NpgsqlCommand(sqlQuery, DataBaseConnection.connection))
             {
-                using (var cmd = new NpgsqlCommand("select * from blackbox", connection))
+                using (var reader = cmd.ExecuteReader())
                 {
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        var dt = new DataTable();
-                        dt.Load(reader);
-                        dataGridView1.DataSource = dt;
-                    }
+                    var dt = new DataTable();
+                    dt.Load(reader);
+                    dataGridView1.DataSource = dt;
+                    textBox1.Text = currentPage.ToString();
                 }
             }
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+            showReport();
+        }
+
+        private void prevPageBtn_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+            }
+            else
+            {
+                return;
+            }
+            showReport(); // Перезагрузка данных с новым номером страницы
+        }
+
+        private void nextPageBtn_Click(object sender, EventArgs e)
+        {
+            currentPage++;
+            showReport(); // Перезагрузка данных с новым номером страницы
         }
     }
 }
